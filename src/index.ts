dadb4c09440fa07968d5029e507f1259885aaf9b
Callum <callummcluskey100@gmail.com>
2022-10-02 17:04:25 +0100

    moved imports to an index.ts file, resolved onpress issue

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { EspruinoTool } from './espruino-tool';
+import { Puck } from './puck/puck';
+
+export default EspruinoTool;
+
+export { Puck };
--- src/puck/puck.ts
+++ src/puck/puck.ts
@@ -4,8 +4,8 @@ import { IPuck } from './types/puck-types';
 
 export class Puck extends EspruinoTool implements IPuck {
   onPress(func: Function) {
+    this.UART.write('reset();\n');
     this.UART.write(`
-    reset();
     setWatch(function(){
       ${stringifyFunction(func)};
     }, BTN,{edge:"rising", repeat:true, debounce:50})
