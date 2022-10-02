b77b272a80444b728f36d641a8b1097a8d8e806c
Callum <callummcluskey100@gmail.com>
2022-10-02 19:29:25 +0100

    partially added base class methods

--- src/espruino-tool.ts
+++ src/espruino-tool.ts
@@ -1,4 +1,5 @@
 import uart from 'espruino-ble-uart';
+import { stringifyFunction } from './puck/helpers/funcToString';
 import { IEspruinoTool } from './types/espruino-tool-types';
 
 export class EspruinoTool implements IEspruinoTool {
@@ -8,6 +9,51 @@ export class EspruinoTool implements IEspruinoTool {
     this.connected = false;
     this.UART = uart;
   }
+  upload(url: string): void {
+    throw new Error('Method not implemented.');
+  }
+  eval(func: Function | string) {
+    const promise = new Promise((resolve) => {
+      let callback = (data: any) => {
+        resolve(data);
+      };
+
+      this.UART.eval(
+        typeof func == typeof Function
+          ? stringifyFunction(func as Function)
+          : func,
+        callback,
+      );
+    }).catch((err) => {
+      throw Error(err);
+    });
+
+    return promise;
+  }
+  async dump(): Promise<string> {
+    let str: Promise<string> = (await this.eval(
+      'E.dumpstr();\n',
+    )) as Promise<string>;
+    return str;
+  }
+  async getDeviceType() {
+    return await this.UART.eval(`proccess.env.BOARD`, function (t: any) {
+      // find a way to pass this data back.
+      console.log(t);
+    });
+  }
+  async getBattery() {
+    return await this.UART.eval(`E.getBattery()`, function (t: any) {
+      // find a way to pass this data back.
+      console.log(t);
+    });
+  }
+  async getTemperature() {
+    return await this.UART.eval(`E.getTemperature()`, function (t: any) {
+      // find a way to pass this data back.
+      console.log(t);
+    });
+  }
 
   connect(): void {
     this.UART?.write('\x03', () => (this.connected = true));
@@ -15,5 +61,11 @@ export class EspruinoTool implements IEspruinoTool {
   disconnect() {
     this.UART?.close();
     this.connected = false;
+  }
+  reset() {
+    this.UART.reset();
+  }
+  write(code: string) {
+    this.UART.write(code);
   }
 }
--- src/types/espruino-tool-types.ts
+++ src/types/espruino-tool-types.ts
@@ -3,4 +3,12 @@ export interface IEspruinoTool {
   UART: any;
   connect(): void;
   disconnect(): void;
+  reset(): void;
+  write(code: string): void;
+  upload(url: string): void;
+  eval(func: Function): any;
+  dump(): void;
+  getDeviceType(): any;
+  getBattery(): any;
+  getTemperature(): any;
 }
