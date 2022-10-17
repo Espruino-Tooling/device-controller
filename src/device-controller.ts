a9545115f7346f4f156f5419af855acac620eae9
Callum McLuskey <callummcluskey100@gmail.com>
2022-10-17 21:54:39 +0100

    fixed silly mistake

--- src/device-controller.ts
+++ src/device-controller.ts
@@ -68,7 +68,7 @@ export class DeviceController implements IDeviceController {
    */
   async connect(callback: Function) {
     if (!this.connected) {
-      await this.connect(() => {
+      await this.UART.connect(() => {
         this.connected = true;
         this.UART.write('digitalPulse(LED2,1,100);\n');
         this.getDeviceType().then((type: string) => {
