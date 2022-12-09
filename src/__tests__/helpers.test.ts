329b7f46b96f8efcf175479d86ee4af5a3a0167d
Callum McLuskey <callummcluskey100@gmail.com>
2022-12-09 14:03:58 +0000

    added all tests for device controller

--- src/__tests__/helpers.test.ts
+++ src/__tests__/helpers.test.ts
@@ -1,1 +1,12 @@
-// miniParser check.
+import { miniEspParser } from '../helpers/funcToString';
+import { Puck } from '../puck';
+
+describe('convert code', () => {
+  it('should convert code from espruino-tools to native espruino', () => {
+    let puck = new Puck();
+    let code = () => {
+      puck.LED.on('red');
+    };
+    expect(miniEspParser(code).trimStart()).toBe('LED1.set();');
+  });
+});
--- src/__tests__/puck.test.ts
+++ src/__tests__/puck.test.ts
@@ -1,19 +1,21 @@
-// getLightVal
+import { PuckMock } from './assets/PuckMock';
+import { UartMock } from './assets/UARTMock';
+import { JSDOM } from 'jsdom';
 
-// mag
+const dom = new JSDOM(`<!doctype html><html><body></body></html>`);
 
-// accel
+global.document = dom.window.document;
+global.window = dom.window as any;
+global.navigator = global.window.navigator;
 
-// IR
+describe('get device type', () => {
+  it('should return the type of device', () => {
+    let device = PuckMock;
 
-// LED
-
-// NFC
-
-// getTemp
-
-// onPress
-
-// onTimedPress
-
-//
+    device.getTemperature().then(() => {
+      expect((device.UART as typeof UartMock).writtenData).toBe(
+        'E.getTemperature();',
+      );
+    });
+  });
+});
